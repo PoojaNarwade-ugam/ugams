f3ec43e382955fdaa1e3111914f94969eba0d889
Pooja Narwade <pooja.narwade@ugamsolutions.com>
2022-01-19 19:39:55 +0530

    deleted

--- core/src/main/java/com/Ugams/core/models/MultiApi.java
+++
@@ -1,9 +0,0 @@
-package com.Ugams.core.models;
-import org.json.JSONException;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-public interface MultiApi {
-    public List<Map<String,String>> getData() throws JSONException, IOException;
-    public String getUrl();
-}
